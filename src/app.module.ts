9534bfe1afb5368b23efaf5113d2372c61883440
lazaroa1 <lazaroalves36@gmail.com>
2023-03-18 13:31:26 -0300

    config and create user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UserController],
+  providers: [PrismaService],
 })
 export class AppModule {}
