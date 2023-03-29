183f7191e2f71944d5d4c5e0341ea0f6961fe6f9
lazaroa1 <lazaroalves36@gmail.com>
2023-03-29 16:34:01 -0300

    create service to users

--- src/app/users/user.controller.ts
+++ src/app/users/user.controller.ts
@@ -1,60 +1,23 @@
 import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
 import { CreateUserBody } from './dto/create.user.body';
-import { NotFoundException } from '@nestjs/common/exceptions';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly prisma: PrismaService) {}
-
-  private async findUser(id: number) {
-    const user = await this.prisma.user.findUnique({
-      where: { id },
-    });
-
-    if (!user) throw new NotFoundException('User not found');
-
-    return user;
-  }
+  constructor(private readonly user: UserService) {}
 
   @Post()
   async create(@Body() body: CreateUserBody) {
-    const { name, email, password } = body;
-
-    return await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
+    return this.user.creat(body);
   }
 
   @Get(':id')
   async show(@Param('id') id: string) {
-    const user = await this.findUser(parseInt(id));
-
-    return user;
+    return this.user.fetch(id);
   }
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() body: CreateUserBody) {
-    const numberId = parseInt(id);
-    await this.findUser(numberId);
-    const { email, password, name } = body;
-
-    const newData: CreateUserBody = {
-      email,
-      password,
-      name,
-      updatedAt: new Date(),
-    };
-
-    const updateUser = await this.prisma.user.update({
-      where: { id: numberId },
-      data: newData,
-    });
-
-    return updateUser;
+    return this.update(id, body);
   }
 }
---
+++ src/app/users/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserBody } from './dto/create.user.body';
+import { NotFoundException } from '@nestjs/common/exceptions';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findUser(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+
+  async creat(user: CreateUserBody) {
+    const { name, email, password } = user;
+
+    return await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  }
+
+  async fetch(id: string) {
+    const user = await this.findUser(id);
+
+    return user;
+  }
+
+  async update(id: string, user: CreateUserBody) {
+    await this.findUser(id);
+    const { email, password, name } = user;
+
+    const newData: CreateUserBody = {
+      email,
+      password,
+      name,
+      updatedAt: new Date(),
+    };
+
+    const updateUser = await this.prisma.user.update({
+      where: { id: Number(id) },
+      data: newData,
+    });
+
+    return updateUser;
+  }
+}
